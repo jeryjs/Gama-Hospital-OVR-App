bd4f11deb9eccde60313b9d521981a04ef77a4d7
Jery <jery99961@gmail.com>
2026-03-18 09:24:23 +0530

    feat(deployment): add ecosystem configuration for application deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+require('dotenv').config(); // loads .env in this folder
+
+module.exports = {
+    apps: [
+        {
+            name: 'ovr-app',
+            script: 'pnpm',
+            args: 'start',
+            env: { PORT: 5000, NODE_ENV: 'development' },
+            env_production: {
+                NODE_ENV: "production",
+                PORT: "6203",
+                NEXTAUTH_URL: "https://gama-ovr.jeryjs.dev"
+            },
+            exec_mode: "fork",
+            autorestart: true,
+            max_restarts: 10,
+            restart_delay: 5000,
+            error_file: process.env.DEPLOY_PATH + "/logs/error.log",
+            out_file: process.env.DEPLOY_PATH + "/logs/out.log",
+            log_date_format: "YYYY-MM-DD HH:mm Z",
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: process.env.DEPLOY_SSH_USER,
+            host: process.env.DEPLOY_HOST,
+            ref: 'origin/main',
+            repo: process.env.DEPLOY_REPO,
+            path: process.env.DEPLOY_PATH,
+            'post-deploy': 'git pull && pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production',
+        },
+    },
+};
