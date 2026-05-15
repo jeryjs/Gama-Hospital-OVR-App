e1ac6b853b948b26e1bc75eaa400c5153c8b0de2
Jery <jery99961@gmail.com>
2026-05-15 08:18:53 +0530

    fix: remove inter font for server build & add iis config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,7 @@ import { AuthProvider } from "@/components/AuthProvider";
 import { SWRProvider } from "@/components/SWRProvider";
 import { ThemeRegistry } from "@/components/ThemeRegistry";
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
 import "./globals.css";
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: "OVR System - Gama Hospital",
@@ -26,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body>
         <ThemeRegistry>
           <AuthProvider>
             <SWRProvider>
